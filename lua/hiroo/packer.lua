2025ed8231059063c3aa710af64061eadc5fd704
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-09-28 13:01:41 +0200

    fix: resolved treesitter problem  - added toml parser

--- lua/hiroo/packer.lua
+++ lua/hiroo/packer.lua
@@ -1,6 +1,14 @@
--- this file can be loaded by calling `lua require('plugins')` from your init.vim
--- only required if you have packer configured as `opt`
-vim.cmd [[packadd packer.nvim]]
+-- ensure the packer plugin manager is installed
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+    vim.cmd([[packadd packer.nvim]])
+    return true
+  end
+  return false
+end
 
 return require('packer').startup(function(use)
 	-- Packer can manage itself
@@ -102,3 +110,9 @@ return require('packer').startup(function(use)
 	}
 }
 end)
+
+-- the first run will install packer and our plugins
+if packer_bootstrap then
+  require("packer").sync()
+  return
+end
