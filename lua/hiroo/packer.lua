2600c7837032a34d5f0ad99fe5c3aa16953c7531
HirooHG <hugo.golliet@etu.univ-smb.fr>
2023-10-20 17:13:26 +0200

    chore: deleted pdf handling & reorganized packer file

--- lua/hiroo/packer.lua
+++ lua/hiroo/packer.lua
@@ -33,11 +33,9 @@ return require('packer').startup(function(use)
 	-- Packer can manage itself
 	use 'wbthomason/packer.nvim'
 
-	use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	  requires = { {'nvim-lua/plenary.nvim'} }
-	}
+  use ('nvim-lua/plenary.nvim')
+  use ('nvim-lua/popup.nvim')
+	use ('nvim-telescope/telescope.nvim')
 	use({
 		'morhetz/gruvbox',
 		as = 'gruvbox',
@@ -52,6 +50,56 @@ return require('packer').startup(function(use)
 	use ('mbbill/undotree')
 	use ('tpope/vim-fugitive')
 
+  -- Neovim tree
+  use {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v2.x",
+    requires = { 
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+      {
+          -- only needed if you want to use the commands with "_with_window_picker" suffix
+          's1n7ax/nvim-window-picker',
+          tag = "v1.*",
+          config = function()
+            require'window-picker'.setup({
+              autoselect_one = true,
+              include_current = false,
+              filter_rules = {
+                -- filter using buffer options
+                bo = {
+                  -- if the file type is one of following, the window will be ignored
+                  filetype = { 'neo-tree', "neo-tree-popup", "notify" },
+
+                  -- if the buffer type is one of following, the window will be ignored
+                  buftype = { 'terminal', "quickfix" },
+                },
+              },
+              other_win_hl_color = '#e35e4f',
+            })
+          end,
+        
+      },
+    }
+  }
+
+  -- comments
+  use 'tpope/vim-surround'
+  use {
+    'numToStr/Comment.nvim',
+    config = function() require('Comment').setup() end
+  }
+  -- floating terminal
+  use 'numToStr/FTerm.nvim'
+  
+  -- install without yarn or npm
+  use({
+      "iamcco/markdown-preview.nvim",
+      run = function() vim.fn["mkdp#util#install"]() end,
+  })
+
+  use 'simrat39/rust-tools.nvim'
   -- lsp 
   use ( 'akinsho/flutter-tools.nvim' )
 	use {
@@ -82,58 +130,6 @@ return require('packer').startup(function(use)
 		-- Snippets
 		{'L3MON4D3/LuaSnip'},             -- Required
 		{'rafamadriz/friendly-snippets'}, -- Optional
-
-    -- Neovim tree
-    use {
-      "nvim-neo-tree/neo-tree.nvim",
-      branch = "v2.x",
-      requires = { 
-        "nvim-lua/plenary.nvim",
-        "nvim-tree/nvim-web-devicons",
-        "MunifTanjim/nui.nvim",
-        {
-            -- only needed if you want to use the commands with "_with_window_picker" suffix
-            's1n7ax/nvim-window-picker',
-            tag = "v1.*",
-            config = function()
-              require'window-picker'.setup({
-                autoselect_one = true,
-                include_current = false,
-                filter_rules = {
-                  -- filter using buffer options
-                  bo = {
-                    -- if the file type is one of following, the window will be ignored
-                    filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-
-                    -- if the buffer type is one of following, the window will be ignored
-                    buftype = { 'terminal', "quickfix" },
-                  },
-                },
-                other_win_hl_color = '#e35e4f',
-              })
-            end,
-          
-        },
-      }
-    },
-
-    -- comments
-    use 'tpope/vim-surround',
-    use {
-      'numToStr/Comment.nvim',
-      config = function() require('Comment').setup() end
-    },
-    -- floating terminal
-    use 'numToStr/FTerm.nvim',
-    use 'simrat39/rust-tools.nvim',
-    
-    -- install without yarn or npm
-    use({
-        "iamcco/markdown-preview.nvim",
-        run = function() vim.fn["mkdp#util#install"]() end,
-    }),
-
-    use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install", setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
 	}
 }
 end)
