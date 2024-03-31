77e98b18bbdde3b35453408133b889995ee10ec7
HirooHG <hugo.golliet@etu.univ-smb.fr>
2024-03-31 16:49:18 +0200

    fix(plugins): changed copilot plugin  - added default setup background for notifs  - updated plugins

--- lua/plugins/notifications.lua
+++ lua/plugins/notifications.lua
@@ -4,7 +4,9 @@ return {
     config = function()
       vim.opt.termguicolors = true
       local notify = require("notify")
-      notify.setup()
+      notify.setup({
+        background_colour = "#000000"
+      })
       vim.notify = notify
     end
   }
