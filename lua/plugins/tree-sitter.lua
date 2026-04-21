16e1ae661b7d7bd7b8ad913636dfbc0cfb921933
HirooHG <hugo@hugo-golliet.dev>
2026-04-21 21:13:57 +0200

    fix: tree-sitter archived, new neovim v0.12 built-in treesitter system

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -1,41 +1,49 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
-    build = function()
-      pcall(require("nvim-treesitter.install").update { with_sync = true })
-    end,
-    config = function()
-      require('nvim-treesitter.configs').setup {
-        ensure_installed = {
-          "typescript",
-          "rust",
-          "dart",
-          "toml",
-          "c_sharp",
-          "css",
-          "scss",
-          "dockerfile",
-          "html",
-          "json"
-        },
-        sync_install = false,
-        auto_install = true,
-        highlight = {
-          enable = true,
-          additional_vim_regex_highlighting = false,
-        },
-        ident = { enable = true },
-        rainbow = {
-          enable = true,
-          extended_mode = true,
-          max_file_lines = nil,
-        }
+    branch = "main",
+    build = ":TSUpdate",
+    main = 'nvim-treesitter',
+    opts = {
+      sync_install = false,
+      auto_install = true,
+      rainbow = {
+        enable = true,
+        extended_mode = true,
+        max_file_lines = nil,
       }
+    },
+    init = function()
+      local ensure_installed = {
+        "typescript",
+        "rust",
+        "dart",
+        "toml",
+        "c_sharp",
+        "css",
+        "scss",
+        "dockerfile",
+        "html",
+        "json"
+      }
+      local alreadyInstalled = require('nvim-treesitter.config').get_installed()
+      local parsersToInstall = vim.iter(ensure_installed)
+          :filter(function(parser)
+            return not vim.tbl_contains(alreadyInstalled, parser)
+          end)
+          :totable()
+      require('nvim-treesitter').install(parsersToInstall)
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+          vim.bo.indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+        end,
+      })
     end,
     dependencies = {
-      "nvim-treesitter/nvim-treesitter-textobjects",
+      { "nvim-treesitter/nvim-treesitter-textobjects", branch = "main" },
       "nvim-treesitter/nvim-treesitter-context",
-      "nvim-treesitter/playground",
     },
     event = 'VeryLazy'
   },
